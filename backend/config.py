11fec87a5b7b1a795b837b6b0975208b629d8eff
Ebube-Ochemba <ebube17@gmail.com>
2024-10-07 21:13:43 +0100

    Update database URI environmental var.

--- backend/config.py
+++ backend/config.py
@@ -14,11 +14,10 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     
-    # Enable CORS
     CORS(app)
 
     # Configure the database
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('URI_STRING')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URI_STRING')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Initialize the database with the app
