395c4b552a84b193d08e031d750d6c0e0c7220dd
Ebube-Ochemba <ebube17@gmail.com>
2024-10-07 22:21:45 +0100

    migrations 1. Add migrations for database schema changes 2. adjust config.py accordingly

--- backend/config.py
+++ backend/config.py
@@ -3,13 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
+from flask_migrate import Migrate
+
 import os
 
 # Load environment variables
 load_dotenv()
 
-# Initialize SQLAlchemy (without attaching it to the app)
+# Initialize SQLAlchemy & Flask-Migrate (without attaching it to the app)
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -22,5 +25,8 @@ def create_app():
 
     # Initialize the database with the app
     db.init_app(app)
+    
+    # Initialize Flask-Migrate with the app and the db
+    migrate.init_app(app, db)
 
     return app
