aed6b99160b0c1740c78e8c0f1d3b2e36563e532
Ebube-Ochemba <ebube17@gmail.com>
2024-10-09 01:31:48 +0100

    added global error handlers

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from config import DevelopmentConfig
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -25,5 +25,16 @@ def create_app(config_class=DevelopmentConfig):
 
     # Register the v1 API blueprint
     app.register_blueprint(v1_bp)
+
+    # Global error handlers
+    @app.errorhandler(404)
+    def page_not_found(error):
+        """Handles 404 Not Found error globally"""
+        return jsonify({"error": "Not found"}), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        """Handles 500 Internal Server Error globally"""
+        return jsonify({"error": "Internal server error"}), 500
 
     return app
