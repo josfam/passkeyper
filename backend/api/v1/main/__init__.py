dc55898af7a4a140ac7244d8c7e1ba5d2bbca068
hagarSamy <hs1800@fayoum.edu.eg>
2024-10-10 22:50:40 +0300

    reverting changes to main

--- backend/api/v1/main/__init__.py
+++ backend/api/v1/main/__init__.py
@@ -4,11 +4,9 @@ from flask import Blueprint
 # Import blueprints in this folder
 from .index import index_bp
 from .user import user_bp
-from .password import password_bp
 
 main_bp = Blueprint("main", __name__)
 
 # Register individual blueprints for different routes
 main_bp.register_blueprint(index_bp)
 main_bp.register_blueprint(user_bp)
-main_bp.register_blueprint(password_bp)
