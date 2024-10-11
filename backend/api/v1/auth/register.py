d4c2add11e479ff0bae2c34e7e49d806d5e9330e
Ebube-Ochemba <ebube17@gmail.com>
2024-10-11 01:20:36 +0100

    renamed

--- backend/api/v1/auth/register.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from flask import Blueprint, jsonify
-
-register_bp = Blueprint('register', __name__)
-
-@register_bp.route('/register', methods=['GET'], strict_slashes=False)
-def register():
-    return jsonify({"message": "You are registered!"}), 200
