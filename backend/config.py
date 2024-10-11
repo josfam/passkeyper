ad2f3e6478355eeedd40cab0a637691c0da85b4c
Ebube-Ochemba <ebube17@gmail.com>
2024-10-11 01:47:26 +0100

    session managment update

--- backend/config.py
+++ backend/config.py
@@ -9,6 +9,8 @@ class Config:
     """Base Config Class"""
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SESSION_PERMANENT = True
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
 
 
 class DevelopmentConfig(Config):
