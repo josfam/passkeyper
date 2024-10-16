3c7940ac1dc71ff73137e8d5240722799cc9aeb3
Ebube-Ochemba <ebube17@gmail.com>
2024-10-16 11:00:19 +0100

    removed new imports as blueprint is incomplete

--- backend/api/v1/auth/__init__.py
+++ backend/api/v1/auth/__init__.py
@@ -4,11 +4,11 @@ from flask import Blueprint
 # Import blueprints in this folder
 from .signup import signup_bp
 from .login import login_bp
-from .OAuth import oauth_bp
+# from .OAuth import oauth_bp
 
 auth_bp = Blueprint("auth", __name__)
 
 # Register individual blueprints for different routes
 auth_bp.register_blueprint(signup_bp)
 auth_bp.register_blueprint(login_bp)
-auth_bp.register_blueprint(oauth_bp)
+# auth_bp.register_blueprint(oauth_bp)
