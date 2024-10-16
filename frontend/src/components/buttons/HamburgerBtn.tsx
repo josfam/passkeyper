694b54ae6497f58f91d9e5421e676c410e0e23f9
josfam <jamukun@proton.me>
2024-10-16 19:45:31 +0300

    Fix: Make sidebar & content separately scrollable
    - The sidebar and the content area on the right do not scroll in tandem
      anymore, but rather independently.
    - This fixes the scenario where the sidebar had a gap underneath it.

--- frontend/src/components/buttons/HamburgerBtn.tsx
+++ frontend/src/components/buttons/HamburgerBtn.tsx
@@ -14,9 +14,9 @@ const HamburgerBtn: React.FC<HamburgerBtnProps> = ({ setIsSidebarOpen, isSidebar
 			flex items-center justify-center'
 			onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
 			<svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M20 7L4 7" stroke="#1C274C" stroke-width="2" strokeLinecap="round"/>
-				<path d="M20 12L4 12" stroke="#1C274C" stroke-width="2" strokeLinecap="round"/>
-				<path d="M20 17L4 17" stroke="#1C274C" stroke-width="2" strokeLinecap="round"/>
+				<path d="M20 7L4 7" stroke="#1C274C" strokeWidth="2" strokeLinecap="round"/>
+				<path d="M20 12L4 12" stroke="#1C274C" strokeWidth="2" strokeLinecap="round"/>
+				<path d="M20 17L4 17" stroke="#1C274C" strokeWidth="2" strokeLinecap="round"/>
 			</svg>
 			{/* Hamburger Icon */}
 		</button>
