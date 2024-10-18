7c142a1d0d08ddaef8e2882da48e1dae933f3217
hagarSamy <hs1800@fayoum.edu.eg>
2024-10-19 02:31:10 +0300

    handled preflight requests

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from backend.config import DevelopmentConfig
 from backend.models import db, migrate, cors, bcrypt, oauth
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from backend.utils.helpers import check_session, register_google_oauth
 from . import v1_bp
@@ -13,12 +13,21 @@ def create_app(config_class=DevelopmentConfig):
     # Apply configuration from config.py
     app.config.from_object(config_class)
     app.config['SESSION_COOKIE_SAMESITE'] = 'None'  # Use 'Lax' if you only need first-party cookies
-    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['SESSION_COOKIE_SECURE'] = True # change to false to allow http reqs via postman
 
     # Initialize the database, migrate, cors, bcrypt & oauth
     db.init_app(app)
     migrate.init_app(app, db)
-    cors.init_app(app, resources={r"/*": {"origins": app.config['CLIENT_ADDRESS'], "supports_credentials": True}})
+    cors.init_app(app, resources={
+        r"/*": {
+            "origins": app.config['CLIENT_ADDRESS'],"methods": ["GET", "HEAD", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
+            "supports_credentials": True
+            # "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"],
+            # "expose_headers": ["Content-Type", "X-CSRFToken"],
+            # "max_age": 3600,
+            # "send_wildcard": False
+        }
+    })
     bcrypt.init_app(app)
     oauth.init_app(app)
 
@@ -27,12 +36,21 @@ def create_app(config_class=DevelopmentConfig):
 
     # Register the v1 API blueprint
     app.register_blueprint(v1_bp)
+    
+    # Explicit OPTIONS handler for preflight requests
+    # @app.route('/', methods=['OPTIONS'])
+    # @app.route('/<path:path>', methods=['OPTIONS'])
+    # def handle_options(path=None):
+    #     """Handles preflight requests for all routes."""
+    #     return '', 204
 
     # Apply session check globally
     @app.before_request
     def before_request():
         """Run before every request to check if the user is logged in."""
-        return check_session()
+        if request.method != 'OPTIONS':  # Skip session check for OPTIONS
+            return check_session()
+        return None
 
     # Global error handlers
     @app.errorhandler(404)
