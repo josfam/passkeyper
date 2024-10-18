3291c5c98598ab73575d4494e4f18e421e9ffaff
hagarSamy <hs1800@fayoum.edu.eg>
2024-10-18 22:56:20 +0300

    user get and update funcs

--- backend/api/v1/main/user.py
+++ backend/api/v1/main/user.py
@@ -1,8 +1,41 @@
 #!/usr/bin/python3
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
+from backend.models.user import User
 
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/user', methods=['GET'], strict_slashes=False)
-def greet_user():
-    return jsonify({"message": "Welcome user!"}), 200
+def get_user():
+    '''retrieving user data'''
+    user_id = session.get('user_id')
+
+    user = User.query.filter_by(id=user_id).first()
+
+    if user:
+        userData = {'name': user.username, 'email': user.email}
+        return jsonify(userData), 200
+
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@user_bp.route('/user', methods=["PATCH"], strict_slashes=False)
+def update_user():
+    '''update user data (name - email)'''
+    user_id = session.get('user_id')
+    
+    data = request.get_json()
+
+    user = User.query.filter_by(id=user_id).first()
+
+    if user:
+        if 'name' in data:
+            user.username = data['name']
+        if 'email' in data:
+            user.email = data['email']
+
+        user.updated_at = func.now()
+
+        db.session.commit()
+        return jsonify({"message": "User updated successfully"}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
