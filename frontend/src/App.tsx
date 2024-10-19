a767f0b801346bfc4486e5fa6be20d617cbc2543
sanotogii <nasaereh@gmail.com>
2024-10-19 13:01:37 +0200

    refactor: improve code formatting and loading state UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import axios from 'axios';
-import Sidebar from './components/Sidebar';
-import Passwords from './pages/Passwords';
-import Trash from './pages/Trash';
-import ImportExport from './pages/ImportExport';
-import PasswordGenerator from './pages/PasswordGenerator';
-import Settings from './pages/Settings';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import './styles/App.css';
-import './styles/base.css';
-import HamburgerBtn from './components/buttons/HamburgerBtn';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import axios from "axios";
+import Sidebar from "./components/Sidebar";
+import Passwords from "./pages/Passwords";
+import Trash from "./pages/Trash";
+import ImportExport from "./pages/ImportExport";
+import PasswordGenerator from "./pages/PasswordGenerator";
+import Settings from "./pages/Settings";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import "./styles/App.css";
+import "./styles/base.css";
+import HamburgerBtn from "./components/buttons/HamburgerBtn";
 
 const API_URL = import.meta.env.VITE_FLASK_APP_API_URL;
 
@@ -24,7 +29,9 @@ const App: React.FC = () => {
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const response = await axios.get(`${API_URL}/check-auth`, { withCredentials: true });
+        const response = await axios.get(`${API_URL}/check-auth`, {
+          withCredentials: true,
+        });
         setIsAuthenticated(response.data.authenticated);
       } catch {
         setIsAuthenticated(false);
@@ -37,50 +44,93 @@ const App: React.FC = () => {
   }, []);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
   }
 
-  const ProtectedRoute: React.FC<{ element: React.ReactElement }> = ({ element }) => {
+  const ProtectedRoute: React.FC<{ element: React.ReactElement }> = ({
+    element,
+  }) => {
     return isAuthenticated ? element : <Navigate to="/login" replace />;
   };
 
   // check if page requires a top margin to compensate for top header height in mobile
   const shouldNotHaveTopOffset = (): boolean => {
-	return location.pathname === '/login' || location.pathname === '/signup';
-  }
+    return location.pathname === "/login" || location.pathname === "/signup";
+  };
 
   return (
     <Router>
-      <div id='app-container' className='flex flex-col h-screen overflow-hidden md:flex-row'>
+      <div
+        id="app-container"
+        className="flex flex-col h-screen overflow-hidden md:flex-row"
+      >
         {isAuthenticated && (
-			<div>
-				{/* Hamburger button for small screens */}
-				<div className='z-30 h-16 p-4 flex w-full bg-slate-200 fixed
-					md:hidden'>
-					<HamburgerBtn setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen}/>
-				</div>
-				{/* Backdrop */}
-				{isSidebarOpen && (
-					<div
-						className='fixed inset-0 bg-black bg-opacity-50 z-20'
-						onClick={() => setIsSidebarOpen(false)} // Close sidebar on backdrop click
-					/>
-				)}
-				{/* Sidebar */}
-				<Sidebar isOpen={isSidebarOpen} />
-			</div>
-		)}
-        <div id='content-area' className={`bg-white min-h-screen flex-1 flex-col overflow-y-scroll
-			${shouldNotHaveTopOffset() ? 'mt-0': 'mt-16 md:mt-0'}`}>
+          <div>
+            {/* Hamburger button for small screens */}
+            <div
+              className="z-30 h-16 p-4 flex w-full bg-slate-200 fixed
+					md:hidden"
+            >
+              <HamburgerBtn
+                setIsSidebarOpen={setIsSidebarOpen}
+                isSidebarOpen={isSidebarOpen}
+              />
+            </div>
+            {/* Backdrop */}
+            {isSidebarOpen && (
+              <div
+                className="fixed inset-0 bg-black bg-opacity-50 z-20"
+                onClick={() => setIsSidebarOpen(false)} // Close sidebar on backdrop click
+              />
+            )}
+            {/* Sidebar */}
+            <Sidebar isOpen={isSidebarOpen} />
+          </div>
+        )}
+        <div
+          id="content-area"
+          className={`bg-white min-h-screen flex-1 flex-col overflow-y-scroll
+			${shouldNotHaveTopOffset() ? "mt-0" : "mt-16 md:mt-0"}`}
+        >
           <Routes>
-            <Route path='/login' element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/passwords' element={<ProtectedRoute element={<Passwords />} />} />
-            <Route path="/trash" element={<ProtectedRoute element={<Trash />} />} />
-            <Route path='/import-export' element={<ProtectedRoute element={<ImportExport />} />} />
-            <Route path='/generator' element={<ProtectedRoute element={<PasswordGenerator />} />} />
-            <Route path='/settings' element={<ProtectedRoute element={<Settings />} />} />
-            <Route path="*" element={<Navigate to={isAuthenticated ? "/passwords" : "/login"} replace />} />
+            <Route
+              path="/login"
+              element={<Login setIsAuthenticated={setIsAuthenticated} />}
+            />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/passwords"
+              element={<ProtectedRoute element={<Passwords />} />}
+            />
+            <Route
+              path="/trash"
+              element={<ProtectedRoute element={<Trash />} />}
+            />
+            <Route
+              path="/import-export"
+              element={<ProtectedRoute element={<ImportExport />} />}
+            />
+            <Route
+              path="/generator"
+              element={<ProtectedRoute element={<PasswordGenerator />} />}
+            />
+            <Route
+              path="/settings"
+              element={<ProtectedRoute element={<Settings />} />}
+            />
+            <Route
+              path="*"
+              element={
+                <Navigate
+                  to={isAuthenticated ? "/passwords" : "/login"}
+                  replace
+                />
+              }
+            />
           </Routes>
         </div>
       </div>
--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,25 +1,31 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import PasswordChangeModal from './PasswordChangeModal.tsx';
-import { FaTrash, FaUser, FaEnvelope, FaKey } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PasswordChangeModal from "./PasswordChangeModal.tsx";
+import { FaTrash, FaUser, FaEnvelope, FaKey } from "react-icons/fa";
 
 const Settings = () => {
-  const [userData, setUserData] = useState({ name: '', email: '' });
+  const [userData, setUserData] = useState({ name: "", email: "" });
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false); // State for edit modal
-  const [newName, setNewName] = useState('');
-  const [newEmail, setNewEmail] = useState('');
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:5000/user', { withCredentials: true });
+        const response = await axios.get("http://127.0.0.1:5000/user", {
+          withCredentials: true,
+        });
         setUserData({ name: response.data.name, email: response.data.email });
       } catch (err: any) {
-        setError(err.response?.data?.message || err.message || 'An error occurred while fetching user data');
+        setError(
+          err.response?.data?.message ||
+            err.message ||
+            "An error occurred while fetching user data"
+        );
       } finally {
         setLoading(false);
       }
@@ -28,22 +34,35 @@ const Settings = () => {
   }, []);
 
   const handleDeleteAccount = () => {
-    alert('Account deleted successfully!');
+    alert("Account deleted successfully!");
     setShowDeleteConfirmation(false);
   };
 
   const handleEditUserData = async () => {
     try {
       // Send request to update user data
-      await axios.patch('http://127.0.0.1:5000/user', { name: newName, email: newEmail }, { withCredentials: true });
+      await axios.patch(
+        "http://127.0.0.1:5000/user",
+        { name: newName, email: newEmail },
+        { withCredentials: true }
+      );
       setUserData({ name: newName, email: newEmail });
       setShowEditModal(false);
     } catch (err: any) {
-      setError(err.response?.data?.message || err.message || 'An error occurred while updating user data');
+      setError(
+        err.response?.data?.message ||
+          err.message ||
+          "An error occurred while updating user data"
+      );
     }
   };
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
   if (error) return <p>Error: {error}</p>;
 
   return (
@@ -55,8 +74,16 @@ const Settings = () => {
         <FaUser className="mr-2" />
         <div>
           <p className="text-sm font-medium text-gray-500">Name</p>
-          <p className="text-xl rounded-lg bg-gray-50 font-semibold">{userData.name}</p>
-          <button className="text-blue-600 underline mt-1" onClick={() => { setNewName(userData.name); setShowEditModal(true); }}>
+          <p className="text-xl rounded-lg bg-gray-50 font-semibold">
+            {userData.name}
+          </p>
+          <button
+            className="text-blue-600 underline mt-1"
+            onClick={() => {
+              setNewName(userData.name);
+              setShowEditModal(true);
+            }}
+          >
             Edit
           </button>
         </div>
@@ -64,16 +91,24 @@ const Settings = () => {
 
       {/* EMAIL */}
       <div className="mb-4 flex items-center">
-      <FaEnvelope className="mr-2" />
+        <FaEnvelope className="mr-2" />
         <div>
           <p className="text-sm font-medium text-gray-500">Email</p>
-          <p className="text-xl rounded-lg bg-gray-50 font-semibold">{userData.email}</p>
-          <button className="text-blue-600 underline mt-1" onClick={() => { setNewEmail(userData.email); setShowEditModal(true); }}>
+          <p className="text-xl rounded-lg bg-gray-50 font-semibold">
+            {userData.email}
+          </p>
+          <button
+            className="text-blue-600 underline mt-1"
+            onClick={() => {
+              setNewEmail(userData.email);
+              setShowEditModal(true);
+            }}
+          >
             Edit
           </button>
         </div>
       </div>
-      
+
       {/* Change Password Section */}
       <div
         className="text-blue-600 cursor-pointer mt-4 flex items-center"
@@ -84,12 +119,21 @@ const Settings = () => {
       </div>
 
       {/* Password Change Modal */}
-      <PasswordChangeModal isOpen={showModal} onClose={() => setShowModal(false)} />
-    
+      <PasswordChangeModal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+      />
+
       {/* Delete Account Section */}
       <div className="flex items-center mt-6">
-        <FaTrash className="text-red-600 h-5 w-5 mr-2 cursor-pointer" onClick={() => setShowDeleteConfirmation(true)} />
-        <span className="text-red-600 cursor-pointer" onClick={() => setShowDeleteConfirmation(true)}>
+        <FaTrash
+          className="text-red-600 h-5 w-5 mr-2 cursor-pointer"
+          onClick={() => setShowDeleteConfirmation(true)}
+        />
+        <span
+          className="text-red-600 cursor-pointer"
+          onClick={() => setShowDeleteConfirmation(true)}
+        >
           Delete Account
         </span>
       </div>
@@ -99,7 +143,10 @@ const Settings = () => {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white rounded-lg p-6 w-80">
             <h2 className="text-lg font-bold mb-4">Confirm Account Deletion</h2>
-            <p className="mb-4">Are you sure you want to delete your account? This action cannot be undone.</p>
+            <p className="mb-4">
+              Are you sure you want to delete your account? This action cannot
+              be undone.
+            </p>
             <div className="flex justify-between">
               <button
                 onClick={handleDeleteAccount}
@@ -124,7 +171,9 @@ const Settings = () => {
           <div className="bg-white rounded-lg p-6 w-80">
             <h2 className="text-lg font-bold mb-4">Edit User Data</h2>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Name</label>
+              <label className="block text-sm font-medium text-gray-700">
+                Name
+              </label>
               <input
                 type="text"
                 value={newName}
@@ -133,7 +182,9 @@ const Settings = () => {
               />
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">Email</label>
+              <label className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
               <input
                 type="email"
                 value={newEmail}
