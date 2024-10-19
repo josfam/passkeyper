f4c04986765c4b966e25694b45d09213127fcc29
Ebube-Ochemba <ebube17@gmail.com>
2024-10-19 18:42:35 +0100

    structure cleanup

--- backend/config.py
+++ backend/config.py
@@ -8,14 +8,16 @@ load_dotenv()
 
 class Config:
     """Base Config Class"""
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI_STRING")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SESSION_TYPE = 'filesystem'
     SESSION_PERMANENT = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
     SESSION_COOKIE_SAMESITE = 'None'  # Use 'Lax' if you only need first-party cookies
     SESSION_COOKIE_SECURE = True # change to false to allow http reqs via postman
-    SECRET_KEY = os.getenv("SECRET_KEY")
+
     CLIENT_ADDRESS = os.getenv("CLIENT_ADDRESS")
     CLIENT_ID = os.getenv("CLIENT_ID")
     CLIENT_SECRET = os.getenv("CLIENT_SECRET")
