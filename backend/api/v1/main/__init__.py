06afd1db521da0ee125e37a7f5d6be42f846f595
hagarSamy <hs1800@fayoum.edu.eg>
2024-10-22 02:34:07 +0300

    salt endpoint

--- backend/api/v1/main/__init__.py
+++ backend/api/v1/main/__init__.py
@@ -6,6 +6,7 @@ from .index import index_bp
 from .user import user_bp
 from .password import password_bp
 from .trash import trash_bp
+from .internalApi import internal_api
 
 main_bp = Blueprint("main", __name__)
 
@@ -14,3 +15,4 @@ main_bp.register_blueprint(index_bp)
 main_bp.register_blueprint(user_bp)
 main_bp.register_blueprint(password_bp)
 main_bp.register_blueprint(trash_bp)
+main_bp.register_blueprint(internal_api)
