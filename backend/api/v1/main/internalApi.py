ff73f286c86de798e77301c5aaef519684a8ee83
hagarSamy <hs1800@fayoum.edu.eg>
2024-10-23 21:51:58 +0300

    updated the return value of the internal route

--- backend/api/v1/main/internalApi.py
+++ backend/api/v1/main/internalApi.py
@@ -8,6 +8,6 @@ def get_ek_salt():
     user_id = session.get('user_id')
     user = User.query.get(user_id)
     if user:
-        return jsonify({'ek_salt': user.ek_salt, 'password': user.hashed_master_password, 'email': user.email}), 200
+        return jsonify({'ek_salt': user.ek_salt, 'password': user.hashed_master_password}), 200
     else:
         return jsonify({"error": "User not found"}), 404
