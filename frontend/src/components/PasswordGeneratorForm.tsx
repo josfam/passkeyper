2121b5b5a01f129e5d332191094ea3544cbe4b8e
josfam <jamukun@proton.me>
2024-10-24 06:43:59 +0300

    Factor out password logic from the general form
    The password logic and UI (that is, the options and length change logic) has
    been moved out of the main password generator form, into a separate
    function.
    
    This makes the generator more easily composable later when a passphrase
    (which also needs its own options) is added to the form.
    This way, the UI can swap out the options section when either password
    or passphrase is selected.

--- frontend/src/components/PasswordGeneratorForm.tsx
+++ frontend/src/components/PasswordGeneratorForm.tsx
@@ -1,34 +1,22 @@
-import { useForm } from 'react-hook-form'
+import { useForm, UseFormReturn } from 'react-hook-form'
 import { Flip, toast } from 'react-toastify'
-import { Input } from '../components/ui/input'
-import { useState , useEffect, useCallback} from 'react'
+import { useState} from 'react'
 import {
 	Form,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormControl,
 } from '../components/ui/form'
-import {
-	MIN_PASSWORD_LEN,
-	MAX_PASSWORD_LEN,} from '../utils/passwords/Constants'
-import generatePassword from '../utils/passwords/GeneratePassword'
+import PasswordOptions from './PasswordOptions'
+import { MIN_PASSWORD_LEN } from '../utils/passwords/Constants'
 
 const PasswordGeneratorForm = () => {
 	// initial state, and functions to change state
 	const [passwordType, setPasswordType] = useState('password')
-	const [hasSpecialChars, setHasSpecialChars] = useState(true)
-	const [hasNumbers, setHasNumbers] = useState(true)
-	const [hasUppercase, setHasUppercase] = useState(true)
-	const [hasLowercase, setHasLowercase] = useState(true)
-	const [passwordLength, setPasswordLength] = useState(MIN_PASSWORD_LEN)
 	const [password, setPassword] = useState('');
 	const [highlightPasswordArea, setHighlightPasswordArea] = useState(false);
-	const [isBadLength, setIsBadLength] = useState(false)
+	const [regenerateTrigger, setRegenerateTrigger] = useState(0);
 
-	const form = useForm({
+	const form: UseFormReturn<{ length: number; }, undefined> = useForm({
 		defaultValues: {
-			length: passwordLength,
+			length: MIN_PASSWORD_LEN,
 		}
 	})
 
@@ -37,29 +25,9 @@ const PasswordGeneratorForm = () => {
 		setPasswordType(e.target.value)
 	}
 
-	// handling state for password length
-	const handlePasswordLengthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setPasswordLength(Number(e.target.value))
-	}
-
-	// handling  state for individual checkboxes for other options
-	const handleOptionsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { id, checked } = e.target
-
-		switch (id) {
-			case 'special-chars':
-				setHasSpecialChars(checked)
-				break;
-			case 'numbers':
-				setHasNumbers(checked)
-				break;
-			case 'uppercase':
-				setHasUppercase(checked)
-				break;
-			case 'lowercase':
-				setHasLowercase(checked)
-				break;
-		}
+	// Force a regeneration of the password by updating regenerate trigger
+	const regeneratePassword = () => {
+		setRegenerateTrigger(prev => prev + 1);
 	}
 
 	// copying the password
@@ -80,26 +48,6 @@ const PasswordGeneratorForm = () => {
 			console.log(err);
 		})	
 	}
-
-	// Memoize the reference to getPassword, unless one of its dependencies changes
-	const getPassword = useCallback((passwordLength: number) => {
-		if (passwordLength < MIN_PASSWORD_LEN || passwordLength > MAX_PASSWORD_LEN) {
-			setIsBadLength(true)
-			return
-		}
-		setIsBadLength(false)
-		if (!hasSpecialChars && !hasUppercase && !hasLowercase && !hasNumbers) {
-			setHasLowercase(true)
-		}
-		const generatedPassword = generatePassword({ passwordLength, hasSpecialChars, hasUppercase, hasLowercase, hasNumbers });
-		setPassword(generatedPassword)
-	}, [hasSpecialChars, hasNumbers, hasUppercase, hasLowercase]);
-
-	// Call get password if password length changes, or if getPassword changed (because one of its
-	// dependencies changed) The useCallback was mostly a work around for eslint errors
-	useEffect(() => {
-		getPassword(passwordLength);
-	}, [getPassword, passwordLength]);
 
 	return (
 		<div className='mx-auto pt-6 px-4'>
@@ -137,77 +85,27 @@ const PasswordGeneratorForm = () => {
 									</div>
 								</div>
 							</div>
-
-							{/* password length */}
-							<FormField
-								control={form.control}
-								name="length"
-								render={({ field }) => (
-									<FormItem className='w-full'>
-										<FormLabel className='text-xl ml-3'>Length</FormLabel>
-										<FormControl>
-											<Input type='number' autoFocus className={`h-12 text-xl border border-slate-400
-												placeholder:text-slate-300 placeholder:font-light hover:border-slate-500 hover:border-2
-												${isBadLength ? ' hover:border-orange-700 border-2 border-orange-700' : ''}`}
-											placeholder='Length of the password' {...field}
-											onChange={(e) => {
-												field.onChange(e); // Track with react hook form
-												handlePasswordLengthChange(e) // custom function
-											}}
-											/>
-										</FormControl>
-										{/* Conditionally render the error message */}
-										{isBadLength && (
-											<p className="text-orange-800 text-md mt-2">
-											{`Password must be between ${MIN_PASSWORD_LEN} and ${MAX_PASSWORD_LEN} characters.`}
-											</p>
-										)}
-									</FormItem>
-								)}
-							/>
-						</div>
-
-						{/* additional options */}
-						<div className='h-fit w-full p-3 flex flex-col gap-2 border border-slate-200 rounded-lg'>
-							<h2 className='text-xl m-0 font-medium'>Options</h2>
-							<hr />
-							<div className='flex flex-col gap-x-8 gap-y-2 flex-wrap md:flex md:flex-row md:gap-y-2'>
-								<div className='flex gap-2'>
-									<input type="checkbox" id="special-chars" name='password-type' value='special-chars'
-									checked={hasSpecialChars}
-									onChange={handleOptionsChange}
-									className='radial-check'/>
-									<label className='text-xl' htmlFor="special-chars">special characters</label>
-								</div>
-								<div className='flex gap-2'>
-									<input type="checkbox" id="numbers" name='password-type' value='numbers'
-									checked={hasNumbers}
-									onChange={handleOptionsChange}
-									className='radial-check'/>
-									<label className='text-xl' htmlFor="numbers">numbers</label>
-								</div>
-								<div className='flex gap-2'>
-									<input type="checkbox" id="uppercase" name='password-type' value='numbers'
-									checked={hasUppercase}
-									onChange={handleOptionsChange}
-									className='radial-check'/>
-									<label className='text-xl' htmlFor="uppercase">uppercase</label>
-								</div>
-								<div className='flex gap-2'>
-									<input type="checkbox" id="lowercase" name='password-type' value='numbers'
-									checked={hasLowercase}
-									onChange={handleOptionsChange}
-									className='radial-check'/>
-									<label className='text-xl' htmlFor="lowercase">lowercase</label>
-								</div>
-							</div>
+							{ passwordType === 'password' ? (
+								<>
+								{/* handle password generation and options */}
+									{<PasswordOptions
+									setPassword={setPassword}
+									form={form}
+									regenerateTrigger={regenerateTrigger} // Force a refresh by updating the trigger
+									/>}
+								</>
+								) : (
+									<>
+									</>
+								)
+							}
 						</div>
 						<div className='w-full flex flex-col-reverse gap-5 md:flex md:flex-row mt-4 sm:mt-0'>
 							<button
 								className='btn-primary btn-secondary sm:max-lg:mb-5'
 								onMouseDown={() => {setHighlightPasswordArea(true)}}
 								onMouseUp={() => {setHighlightPasswordArea(false)}}
-								onClick={() => getPassword(passwordLength)}
+								onClick={() => regeneratePassword()}
 								>
 									Regenerate
 							</button>
---
+++ frontend/src/components/PasswordOptions.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { UseFormReturn } from 'react-hook-form';
+import {
+	MIN_PASSWORD_LEN,
+	MAX_PASSWORD_LEN } from '../utils/passwords/Constants';
+import generatePassword from '../utils/passwords/GeneratePassword';
+import {
+	FormField,
+	FormItem,
+	FormLabel,
+	FormControl,
+} from '../components/ui/form';
+import { Input } from '../components/ui/input'
+
+
+interface passwordOptionProps {
+	setPassword: React.Dispatch<React.SetStateAction<string>>
+	form: UseFormReturn<{ length: number; }, undefined>
+	regenerateTrigger: number
+}
+
+const PasswordOptions = ({ setPassword, form, regenerateTrigger} :passwordOptionProps) => {
+	// initial state, and functions to change state
+	const [hasSpecialChars, setHasSpecialChars] = useState(true)
+	const [passwordLength, setPasswordLength] = useState(MIN_PASSWORD_LEN)
+	const [hasNumbers, setHasNumbers] = useState(true)
+	const [hasUppercase, setHasUppercase] = useState(true)
+	const [hasLowercase, setHasLowercase] = useState(true)
+	const [isBadLength, setIsBadLength] = useState(false)
+
+	// handling  state for individual checkboxes for other options
+	const handleOptionsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { id, checked } = e.target
+
+		switch (id) {
+			case 'special-chars':
+				setHasSpecialChars(checked)
+				break;
+			case 'numbers':
+				setHasNumbers(checked)
+				break;
+			case 'uppercase':
+				setHasUppercase(checked)
+				break;
+			case 'lowercase':
+				setHasLowercase(checked)
+				break;
+		}
+	}
+
+	// Memoize the reference to getPassword, unless one of its dependencies changes
+	const getPassword = useCallback((passwordLength: number) => {
+		if (passwordLength < MIN_PASSWORD_LEN || passwordLength > MAX_PASSWORD_LEN) {
+			setIsBadLength(true)
+			return
+		}
+		setIsBadLength(false)
+		if (!hasSpecialChars && !hasUppercase && !hasLowercase && !hasNumbers) {
+			setHasLowercase(true)
+		}
+		const generatedPassword = generatePassword({ passwordLength, hasSpecialChars, hasUppercase, hasLowercase, hasNumbers });
+		setPassword(generatedPassword)
+	}, [hasSpecialChars, hasNumbers, hasUppercase, hasLowercase, setPassword]);
+
+	useEffect(() => {
+		getPassword(passwordLength);
+	}, [getPassword, passwordLength, regenerateTrigger])
+
+	return (
+		<div className='flex flex-col gap-6'>
+			{/* password length */}
+			<FormField
+				control={form.control}
+				name="length"
+				render={({ field }) => (
+					<FormItem className='w-full'>
+						<FormLabel className='text-xl ml-3'>Length</FormLabel>
+						<FormControl>
+							<Input type='number' autoFocus className={`h-12 text-xl border border-slate-400
+								placeholder:text-slate-300 placeholder:font-light hover:border-slate-500 hover:border-2
+								${isBadLength ? ' hover:border-orange-700 border-2 border-orange-700' : ''}`}
+							placeholder='Length of the password' {...field}
+							onChange={(e) => {
+								field.onChange(e); // Track with react hook form
+								setPasswordLength(Number(e.target.value)); // custom function
+							}}
+							/>
+						</FormControl>
+						{/* Conditionally render the error message */}
+						{isBadLength && (
+							<p className="text-orange-800 text-md mt-2">
+							{`Password must be between ${MIN_PASSWORD_LEN} and ${MAX_PASSWORD_LEN} characters.`}
+							</p>
+						)}
+					</FormItem>
+				)}
+			/>
+			{/* additional options */}
+			<div className='h-fit w-full p-3 flex flex-col gap-2 border border-slate-200 rounded-lg'>
+				<h2 className='text-xl m-0 font-medium'>Options</h2>
+				<hr />
+				<div className='flex flex-col gap-x-8 gap-y-2 flex-wrap md:flex md:flex-row md:gap-y-2'>
+					<div className='flex gap-2'>
+						<input type="checkbox" id="special-chars" name='password-type' value='special-chars'
+						checked={hasSpecialChars}
+						onChange={handleOptionsChange}
+						className='radial-check'/>
+						<label className='text-xl' htmlFor="special-chars">special characters</label>
+					</div>
+					<div className='flex gap-2'>
+						<input type="checkbox" id="numbers" name='password-type' value='numbers'
+						checked={hasNumbers}
+						onChange={handleOptionsChange}
+						className='radial-check'/>
+						<label className='text-xl' htmlFor="numbers">numbers</label>
+					</div>
+					<div className='flex gap-2'>
+						<input type="checkbox" id="uppercase" name='password-type' value='numbers'
+						checked={hasUppercase}
+						onChange={handleOptionsChange}
+						className='radial-check'/>
+						<label className='text-xl' htmlFor="uppercase">uppercase</label>
+					</div>
+					<div className='flex gap-2'>
+						<input type="checkbox" id="lowercase" name='password-type' value='numbers'
+						checked={hasLowercase}
+						onChange={handleOptionsChange}
+						className='radial-check'/>
+						<label className='text-xl' htmlFor="lowercase">lowercase</label>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default PasswordOptions;
